c60e4eeb93cc14d749db8e73dd7dfc4116e3e868
esp-nishikawa <39733568+esp-nishikawa@users.noreply.github.com>
2018-11-20 13:48:55 +0900

    メール件名と自動返信メール本文の修正

--- contact.php
+++ contact.php
@@ -88,6 +88,9 @@ if (!is_bool($privacy) || !$privacy) {
 // 管理者アドレス
 $mail_admin = $type == 1 ? 'saiyou@'.$domain : 'info@'.$domain;
 
+// メール件名
+$mail_subject = '【お問い合わせ】'.$types[$type];
+
 // メール本文
 $mail_body = "ホームページよりお問い合わせがありました。\n";
 $mail_body .= "\n------------------------------------------------------";
@@ -102,20 +105,27 @@ $mail_body .= "\n".date('Y/m/d (D) H:i:s');
 $mail_body .= "\n";
 
 // メール送信
-if (!send_mail($email, $mail_admin, $mail_admin, 'ホームページよりお問い合わせ', $mail_body)) {
-    output_log("メール送信エラー"."\n■from\n".$email."\n■to\n".$mail_admin."\n■body\n".$mail_body, $request_body);
+if (!send_mail($email, $mail_admin, $mail_admin, $mail_subject, $mail_body)) {
+    output_log("メール送信エラー"."\n■from\n".$email."\n■to\n".$mail_admin, $request_body);
     exit_error(500);
 }
+
+// 自動返信メール件名
+$remail_subject = '【eSoftPowers】お問い合わせ受付のお知らせ';
 
 // 自動返信メール本文
 $remail_body = $name." 様\n\n";
 $remail_body .= "お問い合わせいただき誠にありがとうございます。\n";
 $remail_body .= "下記のとおりお問い合わせを受け付けました。\n";
 $remail_body .= "\n------------------------------------------------------";
+$remail_body .= "\n[お問い合わせ日時]"."\n".date('Y/m/d (D) H:i:s')."\n";
 $remail_body .= "\n[お問い合わせ種類]"."\n".$types[$type]."\n";
 $remail_body .= "\n[お問い合わせ内容]"."\n".$contents."\n";
 $remail_body .= "------------------------------------------------------\n";
 $remail_body .= "\n確認後、返信させていただきます。少々お待ちください。\n";
+$remail_body .= "\n◇ご注意◇";
+$remail_body .= "\nこのメールはメールフォームよりお問い合わせいただいた方へ自動送信しております。";
+$remail_body .= "\nお急ぎの場合はお電話にてお問い合わせください。\n";
 $remail_body .= "\n================================";
 $remail_body .= "\n株式会社イーソフトパワーズ";
 $remail_body .= "\nTEL: 03-6273-4837";
@@ -125,7 +135,7 @@ $remail_body .= "\n================================";
 $remail_body .= "\n";
 
 // 自動返信メール送信
-send_mail($mail_admin, $email, $mail_admin, 'お問い合わせありがとうございます', $remail_body);
+send_mail($mail_admin, $email, $mail_admin, $remail_subject, $remail_body);
 
 // 正常終了
 $response_body = json_encode(array('result_code' => 'success'));
